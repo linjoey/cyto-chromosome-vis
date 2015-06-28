0fd433239e3ba48bd9be4c41eaaa7c29caa4a95d
Joe Lin <linjoeyy@gmail.com>
2015-06-28 14:30:43 -0400

    configurable selector

--- src/selector.js
+++ src/selector.js
@@ -2,63 +2,80 @@
 (function(cyto_chr, d3) {
 
   var Selector = function(extent) {
-    if(extent){ this.extent = extent; }
-    this.brush = d3.svg.brush();
+    if(extent){ this._extent = extent; }
+    this._brush = d3.svg.brush();
     this.dispatch = d3.dispatch('change');
+    this._x = 0;
+    this._y = 0;
+    this._extent = [0,0];
+    this._height = 0;
+    this._test = "default";
+    this._another = "def";
   };
 
-  //TODO make get work after setting as a function
-  function getSet(prop, arg) {
+  function getSet(prop, arg, cb) {
     if(typeof arg !== 'undefined') {
       this[prop] =  arg;
+      if(typeof cb === 'function') {
+        cb();
+      }
       return this;
     } else {
       return this[prop];
     }
   }
 
-  Selector.prototype.extent = function (e) {
-    getSet.call(this, "extent", e);
-
-    this.brush.extent(e);
-
-    return this;
+  Selector.prototype.test = function(e) {
+    var self = this;
+    return getSet.call(this, "_test", e, function(){
+      self._another = "_that";
+    });
   };
 
-  Selector.prototype.xscale = function(s) {
+  Selector.prototype.extent = function (a) {
 
-    getSet.call(this, "xscale", s);
+    var self = this;
+    return getSet.call(this, "_extent", a, function(){
+      self._brush.extent(a);
+    });
 
-    this.brush.x(this.xscale);
-    return this;
+  };
+
+  Selector.prototype.xscale = function(a) {
+
+    var self = this;
+    return getSet.call(this, "_xscale", a, function(){
+      self._brush.x(a)
+    });
   };
 
   Selector.prototype.target = function(a) {
-    return getSet.call(this, 'target', a);
+    return getSet.call(this, '_target', a);
   };
 
   Selector.prototype.height = function(a) {
-    return getSet.call(this, 'height', a);
+    return getSet.call(this, '_height', a);
   };
 
   Selector.prototype.x = function(a) {
-    return getSet.call(this, 'x', a);
+    return getSet.call(this, '_x', a);
   };
 
   Selector.prototype.y = function(a) {
-    return getSet.call(this, 'y', a);
+    return getSet.call(this, '_y', a);
   };
 
   Selector.prototype.render = function() {
 
     var self = this;
 
-    this.selector = this.target.append('g')
-        .attr('transform', 'translate(' + this.x + ',' + this.y + ')')
-        .call(this.brush);
+    this.selector = this._target.append('g')
+      .classed('selector', true)
+      .attr('transform', 'translate(' + this._x + ',' + this._y + ')')
+      .call(this._brush);
 
     this.selector.selectAll('rect')
-      .attr('height', this.height);
+      .attr('height', this._height);
 
     this.selector.select('.background').remove();
 
@@ -66,33 +83,42 @@
       .style('fill', 'steelblue')
       .style('opacity', '0.5');
 
-    var cbg = this.target.append('g');
+    var cbg_xpos = this._xscale(this._extent[1]) + 5;
+    var cbg_ypos = cyto_chr.margin.top - 3;
+
+    var cbg = this._target.append('g');
     cbg.append('title').text('remove');
 
     this.deleteButton = cbg.append('circle')
-      .attr('cx', this.xscale(this.extent[1]) + 5)
-      .attr('cy', cyto_chr.margin.top - 5)
+      .attr('cx', cbg_xpos)
+      .attr('cy', cbg_ypos)
       .attr('r', 5)
       .attr('fill', 'red')
+      //.style('opacity', '0')
       .on('mouseover', function() {
         d3.select(this)
           .style('cursor', 'pointer')
-          .style('opacity', '0.7');
+          //.style('opacity', '1');
       })
       .on('mouseout', function(){
         d3.select(this)
           .style('cursor', 'default')
-          .style('opacity', '1');
+          //.style('opacity', '0');
       })
       .on('click', function() {
 
         self.remove();
       });
 
-    this.brush.on('brush', function(d) {
-      var e = self.brush.extent();
-      self.deleteButton.attr('cx', self.xscale(e[1]) + 5);
+    this._brush.on('brush', function(d) {
+      var e = self._brush.extent();
+      var new_xpos = self._xscale(e[1]) + 5;
+      self.deleteButton.attr('cx', new_xpos);
       self.dispatch.change(e);
+    });
+
+    this._brush.on('brushend', function(d){
+      //console.log('cld');
     });
 
     return this;
@@ -104,6 +130,8 @@
     return this;
   };
 
-  cyto_chr.Selector = Selector;
+  cyto_chr.selector = function(){
+    return new Selector();
+  };
 
 })(window.cyto_chr = window.cyto_chr || {}, d3);
