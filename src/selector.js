bdec3efa03ba1b56984d790ac1afb3f95efabc34
Joe Lin <linjoeyy@gmail.com>
2015-06-28 20:55:23 -0400

    selector udates

--- src/selector.js
+++ src/selector.js
@@ -1,21 +1,19 @@
 
-(function(cyto_chr, d3) {
+(function(cyto_vis, d3) {
 
-  var Selector = function(extent) {
-    if(extent){ this._extent = extent; }
+  var Selector = function(closecb) {
     this._brush = d3.svg.brush();
     this.dispatch = d3.dispatch('change');
     this._x = 0;
     this._y = 0;
     this._extent = [0,0];
     this._height = 0;
-    this._test = "default";
-    this._another = "def";
+    this._closecb = closecb;
   };
 
   Selector.prototype.test = function(e) {
     var self = this;
-    return cyto_chr.InitGetterSetter.call(this, "_test", e, function(){
+    return cyto_vis.InitGetterSetter.call(this, "_test", e, function(){
       self._another = "_that";
     });
   };
@@ -23,7 +21,7 @@
   Selector.prototype.extent = function (a) {
 
     var self = this;
-    return cyto_chr.InitGetterSetter.call(this, "_extent", a, function(){
+    return cyto_vis.InitGetterSetter.call(this, "_extent", a, function(){
       self._brush.extent(a);
     });
 
@@ -32,25 +30,25 @@
   Selector.prototype.xscale = function(a) {
 
     var self = this;
-    return cyto_chr.InitGetterSetter.call(this, "_xscale", a, function(){
+    return cyto_vis.InitGetterSetter.call(this, "_xscale", a, function(){
       self._brush.x(a)
     });
   };
 
   Selector.prototype.target = function(a) {
-    return cyto_chr.InitGetterSetter.call(this, '_target', a);
+    return cyto_vis.InitGetterSetter.call(this, '_target', a);
   };
 
   Selector.prototype.height = function(a) {
-    return cyto_chr.InitGetterSetter.call(this, '_height', a);
+    return cyto_vis.InitGetterSetter.call(this, '_height', a);
   };
 
   Selector.prototype.x = function(a) {
-    return cyto_chr.InitGetterSetter.call(this, '_x', a);
+    return cyto_vis.InitGetterSetter.call(this, '_x', a);
   };
 
   Selector.prototype.y = function(a) {
-    return cyto_chr.InitGetterSetter.call(this, '_y', a);
+    return cyto_vis.InitGetterSetter.call(this, '_y', a);
   };
 
   Selector.prototype.render = function() {
@@ -71,8 +69,8 @@
       .style('fill', 'steelblue')
       .style('opacity', '0.5');
 
-    var cbg_xpos = this._xscale(this._extent[1]) + 5;
-    var cbg_ypos = cyto_chr.margin.top - 3;
+    var cbg_xpos = this._xscale(this._extent[1]) + cyto_vis.margin.left;
+    var cbg_ypos = cyto_vis.margin.top - 3;
 
     var cbg = this._target.append('g');
     cbg.append('title').text('remove');
@@ -98,16 +96,14 @@
         self.remove();
       });
 
-    this._brush.on('brush', function(d) {
-      var e = self._brush.extent();
-      var new_xpos = self._xscale(e[1]) + 5;
-      self.deleteButton.attr('cx', new_xpos);
+    this._brush.on('brush', function() {
+      self.updateXButton();
       self.dispatch.change(e);
     });
 
-    this._brush.on('brushend', function(d){
-      //console.log('cld');
-    });
+    //this._brush.on('brushend', function(d){
+    //
+    //});
 
     return this;
   };
@@ -115,11 +111,25 @@
   Selector.prototype.remove = function() {
     this.selector.remove();
     this.deleteButton.remove();
+    this._closecb(this);
     return this;
   };
 
-  cyto_chr.selector = function(){
-    return new Selector();
+  Selector.prototype.updateXButton = function() {
+    var e = this._brush.extent();
+    var new_xpos = this._xscale(e[1]) + cyto_vis.margin.left;
+    this.deleteButton.attr('cx', new_xpos);
   };
 
-})(window.cyto_chr = window.cyto_chr || {}, d3);
+  Selector.prototype.move = function(start, stop) {
+
+    this._brush.extent([start, stop]);
+    this.selector.call(this._brush);
+    this.updateXButton();
+  };
+
+  cyto_vis.selector = function(cb){
+    return new Selector(cb);
+  };
+
+})(window.cyto_vis = window.cyto_vis || {}, d3);
